ff99d7b714967eceb16e534efbc381b3eccefd16
VictorNaniii <victor.nani@iis.utm.md>
2025-07-09 15:04:23 +0300

    Add user.pb.go

--- internal/api/user/service.go
+++ internal/api/user/service.go
@@ -1,6 +1,11 @@
 package user
 
-import desc ""
+import (
+	desc "../../../pkg/user_v1"
+	"../../service"
+)
 
 type Implementation struct {
+	desc.UnimplementedUserV1Server
+	userService service.UserService
 }
--- internal/service/user.go
+++ internal/service/user.go
@@ -1,11 +1,1 @@
 package service
-
-import (
-	"../model"
-	"context"
-)
-
-type UserService interface {
-	Create(ctx context.Context, info *model.UserInfo) (string error)
-	Get(ctx context.Context, uuid string) (*model.UserInfo, error)
-}
